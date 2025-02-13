2dd65fa01d2e0f0a0b0e056c596d3ed3b93ecc84
ravikumar <kp.ravikumar04@gmail.com>
2025-02-13 20:59:54 +0530

    initial commit

---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/DataStructuresAlgorithamsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.awsravi.data_structures_algorithams_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresAlgorithamsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructuresAlgorithamsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awsravi/data_structures_algorithams_java/DataStructuresAlgorithamsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsravi.data_structures_algorithams_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresAlgorithamsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
