a36165198e94131bf125fb15d60f68a5431a44e2
ravikumar <kp.ravikumar04@gmail.com>
2025-02-17 01:17:14 +0530

    Traversal Reverse Traversal DoublyLinkedList

--- src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/doublylinkedlist/DoublyLinkedList.java
@@ -19,4 +19,71 @@ public class DoublyLinkedList {
 
     }
 
+    // Insertion Method DoublyLinkedList
+    public void insertDoublyLinkedList(int nodeValue, int location) {
+        DoublyNode newNode = new DoublyNode();
+        newNode.value = nodeValue;
+        if (head == null) {
+            createDoublyLinkedList(nodeValue);
+            return;
+        } else if (location == 0) {
+            newNode.next = head;
+            newNode.prev = null;
+            head.prev = newNode;
+            head = newNode;
+        } else if (location >= size) {
+            newNode.next = null;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        } else {
+            DoublyNode tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            newNode.prev = tempNode;
+            newNode.next = tempNode.next;
+            tempNode.next = newNode;
+            tempNode.next.prev = newNode;
+        }
+        size++;
+
+    }
+
+    // Traversal DoublyLinkedList
+    public void traversalDoublyLinkedList() {
+        if (head != null) {
+            DoublyNode tempNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.next;
+            }
+        } else {
+            System.out.println("Nothing to traverse");
+        }
+        System.out.println("\n");
+    }
+
+    //Traversal Reverse Traversal DoublyLinkedList
+    public void reverseTraversalDoublyLinkedList() {
+        if (head != null) {
+            DoublyNode tempNode = tail;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print(" <- ");
+                }
+                tempNode = tempNode.prev;
+            }
+        } else {
+            System.out.println("Nothing to reverse");
+        }
+        System.out.println("\n");
+
+    }
 }
--- src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/doublylinkedlist/Main.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/doublylinkedlist/Main.java
@@ -3,7 +3,23 @@ package com.awsravi.data_structures_algorithams_java.linkedlist.doublylinkedlist
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dLL = new DoublyLinkedList();
-        dLL.createDoublyLinkedList(4);
-        System.out.println(dLL.head.value);
+        //dLL.createDoublyLinkedList(1);
+        //System.out.println(dLL.head.value);
+
+        //Insert DoublyLinkedList
+        dLL.insertDoublyLinkedList(2, 0);
+        dLL.insertDoublyLinkedList(3, 1);
+        dLL.insertDoublyLinkedList(4, 2);
+        dLL.insertDoublyLinkedList(5, 7);
+        // System.out.println(dLL.head.value);
+        // System.out.println(dLL.head.next.value);
+        // System.out.println(dLL.head.next.next.value);
+        // System.out.println(dLL.head.next.next.next.value);
+
+        //Traversal DoublyLinkedList
+        dLL.traversalDoublyLinkedList();
+
+        // Reverse Traversal DoublyLinkedList
+        dLL.reverseTraversalDoublyLinkedList();
     }
 }
