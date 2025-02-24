610a277cf5d0fd6f6d111f2607b6d787e826f5ae
ravikumar <kp.ravikumar04@gmail.com>
2025-02-24 10:55:42 +0530

    Hashing : Double Hashing Hash-Function,Insert,Search,Delete Methods

--- src/main/java/com/awsravi/data_structures_algorithams_java/hashing/doublehashing/Main.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/hashing/doublehashing/Main.java
@@ -3,6 +3,14 @@ package com.awsravi.data_structures_algorithams_java.hashing.doublehashing;
 public class Main {
     public static void main(String[] args) {
 
-        
+        DoubleHashing doubleHashing = new DoubleHashing(8);
+        doubleHashing.insertKeyInHashTable("jai");
+        doubleHashing.insertKeyInHashTable("karya");
+        doubleHashing.insertKeyInHashTable("siddi");
+        doubleHashing.insertKeyInHashTable("hanuman");
+        doubleHashing.insertKeyInHashTable("arunachala");
+        doubleHashing.insertKeyInHashTable("siva");
+        doubleHashing.displayHashTable();
+
     }
 }
