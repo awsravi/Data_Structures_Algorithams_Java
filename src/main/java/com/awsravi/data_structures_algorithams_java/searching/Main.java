1909d7828a2649dcabce0a357e5faa8ba06600f1
ravikumar <kp.ravikumar04@gmail.com>
2025-02-26 00:11:13 +0530

    linearSearch Searching

---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/searching/Main.java
@@ -0,0 +1,12 @@
+package com.awsravi.data_structures_algorithams_java.searching;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 11, 12, 8, 9, 17, 18, 13, 14, 15, 5, 6, 7, 16, 19};
+        Searching s = new Searching();
+        System.out.println("Before Merge array : " + Arrays.toString(arr));
+        s.linearSearch(arr, 10);
+    }
+}
---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/searching/Searching.java
@@ -0,0 +1,28 @@
+package com.awsravi.data_structures_algorithams_java.searching;
+
+public class Searching {
+    /*
+    Linear Search is a sequential searching algorithm that checks each element in
+    the array one by one until it finds the target element or reaches the end of the array.
+    It is simple but inefficient for large datasets.
+     */
+
+    /*
+    Iterate through the array (for loop).
+    Check if arr[i] is equal to target.
+    If found, print and return the index.
+    If not found, return -1.
+
+     */
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Index found At : " + i);
+                return i;
+            }
+        }
+        System.out.println("Not found");
+        return -1;
+
+    }
+}
