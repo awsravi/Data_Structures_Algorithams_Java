c19b77eef3a89f729e447635f3676b5128eb5aa9
ravikumar <kp.ravikumar04@gmail.com>
2025-02-25 11:02:22 +0530

    BubbleSort

---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/sorting/bubblesort/BubbleSort.java
@@ -0,0 +1,25 @@
+package com.awsravi.data_structures_algorithams_java.sorting.bubblesort;
+
+public class BubbleSort {
+
+    void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
