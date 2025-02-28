b64a8cee2955bd8e3d80afb58e3e8dc4a131c207
ravikumar <kp.ravikumar04@gmail.com>
2025-03-01 01:35:17 +0530

    Additional Sorting ways

--- src/main/java/com/awsravi/data_structures_algorithams_java/sorting/Details.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/sorting/Details.java
@@ -2,9 +2,18 @@ package com.awsravi.data_structures_algorithams_java.sorting;
 
 public class Details {
     /*
-    Timsort,Introsort,Comb Sort
-    Pigeonhole Sort,Cycle Sort,Gnome Sort
-    Bogosort,Slowsort,Sleep Sort
-    Adaptive Sorting,External Sorting
+    Timsort,
+    Introsort,
+    Comb Sort
+
+    Pigeonhole Sort,
+    Cycle Sort,
+    Gnome Sort,
+    Bogosort,
+    Slowsort,
+    Sleep Sort
+
+    Adaptive Sorting,
+    External Sorting
      */
 }
