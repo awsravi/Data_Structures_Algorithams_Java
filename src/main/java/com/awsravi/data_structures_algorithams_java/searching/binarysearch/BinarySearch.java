dafa7af247913740b9b82d53d61168c25d6db5ac
ravikumar <kp.ravikumar04@gmail.com>
2025-02-28 11:59:34 +0530

    binarySearch Searching

---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/searching/binarysearch/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.awsravi.data_structures_algorithams_java.searching.binarysearch;
+
+public class BinarySearch {
+
+    /*
+    Binary search:
+    Binary Search is faster than Linear Search
+    Half of the remaining elements can be eliminated at a time, instead of eliminating them one by one
+    Binary Search only works for sorted arrays.
+     */
+    public int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = (low + high) / 2;
+
+        System.out.println("See : " + low + " " + mid + " " + high);
+        while (arr[mid] != target) {
+            if (target < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+            mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                System.out.println("The element is found at the index: " + mid);
+                return mid;
+            } else {
+                System.out.println("The element " + target + " not found.");
+                return -1;
+            }
+        }
+        return low;
+    }
+}
---
+++ src/main/java/com/awsravi/data_structures_algorithams_java/searching/binarysearch/Main.java
@@ -0,0 +1,11 @@
+package com.awsravi.data_structures_algorithams_java.searching.binarysearch;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 8;
+        bs.binarySearch(arr, target);
+
+    }
+}
