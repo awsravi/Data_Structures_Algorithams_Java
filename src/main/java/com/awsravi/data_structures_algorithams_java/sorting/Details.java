486827f0a7ac618d2091ebac04f681b855bd1bfe
ravikumar <kp.ravikumar04@gmail.com>
2025-03-01 01:36:33 +0530

    Additional Sorting ways

--- src/main/java/com/awsravi/data_structures_algorithams_java/sorting/Details.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/sorting/Details.java
@@ -1,6 +1,9 @@
 package com.awsravi.data_structures_algorithams_java.sorting;
 
 public class Details {
+    public static void main(String[] args) {
+        System.out.println("Sorting types : ");
+    }
     /*
     Timsort,
     Introsort,
