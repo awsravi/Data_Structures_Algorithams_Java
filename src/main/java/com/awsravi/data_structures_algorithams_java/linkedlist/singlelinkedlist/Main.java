c6b4b6e75039e93154c09687e5ec06cd41cb509c
Ravikumar <127386201+awsravi@users.noreply.github.com>
2025-04-02 08:01:20 +0530

    Update Main.java
    name changed

--- src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/singlelinkedlist/Main.java
+++ src/main/java/com/awsravi/data_structures_algorithams_java/linkedlist/singlelinkedlist/Main.java
@@ -1,6 +1,6 @@
 package com.awsravi.data_structures_algorithams_java.linkedlist.singlelinkedlist;
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
 
         SingleLinkedList sLL = new SingleLinkedList();
